9c280626cde7a8388cba74b4f2cf525bef00f579
Jonathan <jonathanponcec@hotmail.com>
2019-04-16 15:23:08 -0500

    Se agrego el login y vista principal

--- src/Migrations/Version20190416174235.php
+++ src/Migrations/Version20190416174235.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190405211321 extends AbstractMigration
+final class Version20190416174235 extends AbstractMigration
 {
     public function getDescription() : string
     {
